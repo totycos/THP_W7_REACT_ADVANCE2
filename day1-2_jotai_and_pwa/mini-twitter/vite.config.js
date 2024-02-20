8e6aba6ea962f6f4194a279abeb91fcd8b36c2ad
totycos <dv94@hotmail.fr>
2024-02-20 12:39:41 +0100

    Commit REACT_ADVANCE2 day 2 - mini-twitter: Add settings for PWA + icons

--- day1-2_jotai_and_pwa/mini-twitter/vite.config.js
+++ day1-2_jotai_and_pwa/mini-twitter/vite.config.js
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
+
+const manifestForPlugin = {
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Y",
+    short_name: "Y",
+    description: "This website is a training to React, global state handling and tokens. Here, authentification and routing will be used to create a small social media website.",
+    icons: [{
+      "src": "images/y-128.png",
+      "sizes": "128x128",
+      "type": "image/png"
+    }, {
+      "src": "images/y-144.png",
+      "sizes": "144x144",
+      "type": "image/png"
+    }, {
+      "src": "images/y-152.png",
+      "sizes": "152x152",
+      "type": "image/png"
+    }, {
+      "src": "images/y-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }, {
+      "src": "images/y-256.png",
+      "sizes": "256x256",
+      "type": "image/png"
+    }, {
+      "src": "images/y-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+    ],
+    theme_color: 'black',
+    background_color: 'black',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  },
+};
+
+
